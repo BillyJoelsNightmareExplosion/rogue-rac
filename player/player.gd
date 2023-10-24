e4e0861e9809a51118bd2eb92fbc0229c322b1e1
alectrem <tremblayalec14@gmail.com>
2023-10-23 22:11:55 -0400

    adds test player model for debug, wip char script

--- player/player.gd
+++ player/player.gd
@@ -16,25 +16,27 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 enum MoveStates {
-    WALK,
-    SPRINT,
-    COMBAT
+	WALK,
+	SPRINT,
+	COMBAT
 }
 
 var move_state = MoveStates.WALK
 var speed = WALK_SPEED
 var erosion_momentum = 0.9
 
+var look_pos_last = Vector3(0,0,-1)
 
 func _input(event):
-    if event.is_action_pressed("attack"):
-        move_state = MoveStates.COMBAT
-    elif event.is_action_pressed("sprint"):
-        move_state = MoveStates.SPRINT
+	if event.is_action_pressed("attack"):
+		move_state = MoveStates.COMBAT
+		print("attack")
+	elif event.is_action_pressed("sprint"):
+		move_state = MoveStates.SPRINT
 
-    #get mouse input for camera rotation
-    if event is InputEventMouseMotion:
-        rotate_y(deg_to_rad(-event.relative.x * LOOK_SENSITVITY  * 0.5))
+	#get mouse input for camera rotation
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * LOOK_SENSITVITY  * 0.5))
 #        head.rotation_degrees.y = clamp(head.rotation_degrees.y, -1 * MAX_HEAD_TURN, MAX_HEAD_TURN)
 #        rotate_y(deg_to_rad(-event.relative.x * LOOK_SENSITVITY))
 #        # code for up/down look
@@ -53,30 +55,35 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_dir = Vector3(input_dir.x, 0, input_dir.y)
 
-    # slows instantly, TODO fix
-    speed = RUN_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
+	# slows instantly, TODO fix
+	speed = RUN_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
 
-    var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
-    var forward_vector = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
-    
-    print(direction)
-                
-    if direction:
-        velocity.x = clamp(velocity.x * erosion_momentum + direction.x, -1 * speed, speed)
-        velocity.z = clamp(velocity.z * erosion_momentum + direction.z, -1 * speed, speed)
-    
-    
-        if move_state != MoveStates.COMBAT:
-            var alignmentFactor = (forward_vector.dot(direction) - 1) / -2
-            # (alignmentFactor * 0.3) + 0.1 the less aligned the new and old direction are,
-            #  the faster we want to turn - long live Squirreling Away
-            look_at(forward_vector.lerp(direction, (alignmentFactor * 0.3) + 0.1) + position)
-    else:
-        velocity.x = move_toward(velocity.x, 0, speed)
-        velocity.z = move_toward(velocity.z, 0, speed)
 
-    move_and_slide()
+	# var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	var direction = (transform.basis * input_dir).normalized()
+	var forward_vector = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+	
+	if direction:
+		velocity.x = clamp(velocity.x * erosion_momentum + direction.x, -1 * speed, speed)
+		velocity.z = clamp(velocity.z * erosion_momentum + direction.z, -1 * speed, speed)
+	
+		if move_state != MoveStates.COMBAT:
+			var alignmentFactor = (forward_vector.dot(direction) - 1) / -2
+			# (alignmentFactor * 0.3) + 0.1 the less aligned the new and old direction are,
+			#  the faster we want to turn - long live Squirreling Away
+			# print(direction)
+			$debug_sphere.position = input_dir
+			$body_collision.look_at($debug_sphere.global_position)
+		else:
+			$body_collision.look_at(global_position + Vector3.FORWARD)
+		# $CollisionShape3D.look_at(forward_vector)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
 
 
 
